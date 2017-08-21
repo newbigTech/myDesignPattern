035b33f1d9d4b9756ac5ed614cb7c9f0790a6585
skylar-yu <745369408@qq.com>
2017-08-21 20:01:46 +0800

    submit code

---
+++ src/com/ytc/dp/adapter/TestIterator.java
@@ -0,0 +1,27 @@
+package com.ytc.dp.adapter;
+
+import java.util.Iterator;
+
+/**
+ * Created by Administrator on 2017/8/21.
+ */
+public class TestIterator {
+    public static void main(String[] args) {
+//        Iterator it = new EumerationAdapter(null);
+//        Object obj = it.next();
+//        ClassLoader c  = TestIterator.class.getClassLoader();  //获取Test类的类加载器
+//
+//        System.out.println(c);
+//
+//        ClassLoader c1 = c.getParent();  //获取c这个类加载器的父类加载器
+//
+//        System.out.println(c1);
+//
+//        ClassLoader c2 = c1.getParent();//获取c1这个类加载器的父类加载器
+//
+//        System.out.println(c2);
+
+//        System.out.println(System.getProperty("java.ext.dirs"));
+//        EumerationAdapter EumerationAdapter = new EumerationAdapter();
+    }
+}
--- src/com/ytc/dp/facade/eg1/Client.java
+++ src/com/ytc/dp/facade/eg1/Client.java
@@ -6,7 +6,7 @@ package com.ytc.dp.facade.eg1;
 public class Client {
     public static void main(String [] args){
 //        new Facade().run();
-        FacadeApi facade = FacadeFactory.getFacade();//run();
+        FacadeApi facade = FacadeFactory.createFacade();//run();
         facade.run();
     }
 }
