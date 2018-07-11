8c50a249470130e10ffb9ba9feb4888ed22294e4
skylar-yu <745369408@qq.com>
2018-07-11 21:58:57 +0800

    uml图

--- src/com/ytc/juc/CachedData.java
+++ src/com/ytc/juc/CachedData.java
@@ -28,7 +28,7 @@ class CachedData {
             }
             // Downgrade by acquiring read lock before releasing write lock
             //锁降级，在释放写锁前获取读锁
-            rwl.readLock().lock();
+            rwl.readLock().lock();  //是否有必要获取？ 有必要
             rwl.writeLock().unlock(); // Unlock write, still hold read
         }
 
