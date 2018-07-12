5dcf7b9428fd69cf96fbc17eb8211ae2fd979577
yutianci <745369408@qq.com>
2018-07-12 08:53:01 +0800

    uml commit

--- src/com/ytc/juc/CachedData.java
+++ src/com/ytc/juc/CachedData.java
@@ -28,12 +28,26 @@ class CachedData {
             }
             // Downgrade by acquiring read lock before releasing write lock
             //锁降级，在释放写锁前获取读锁
-            rwl.readLock().lock();  //是否有必要获取？ 有必要
+            rwl.readLock().lock();  //是否有必要获取？ 有必要。如果此处不获取读锁直接释放写锁，
+                                    // 其他线程在updateData()方法中获取到写锁，更新data，
+                                    // 而当前线程调用use(data)无法感知数据的更新，无法保证数据的可见性
             rwl.writeLock().unlock(); // Unlock write, still hold read
         }
-
         use(data);
         rwl.readLock().unlock();    //释放读锁
+    }
+
+    public void updateData(){
+        try{
+            rwl.writeLock().lock();
+            doUpdateData(data);
+        } finally {
+            rwl.writeLock().unlock();
+        }
+    }
+
+    private void doUpdateData(Object data) {
+
     }
 
     private Object loadDataFromDB() {
