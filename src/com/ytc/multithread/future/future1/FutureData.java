1395c66d9050a1f5ffc21175b3d2d66194639747
skylar-yu <745369408@qq.com>
2018-09-09 13:23:48 +0800

    move to future2 package

--- src/com/ytc/multithread/future/future1/FutureData.java
+++ src/com/ytc/multithread/future/future1/FutureData.java
@@ -3,7 +3,7 @@
  * All Rights Reserved.
  * 本软件为深圳万普拉斯科技有限公司开发研制。未经本公司正式书面同意，其他任何个人、团体不得使用、复制、修改或发布本软件.
  *****************************************************************************/
-package com.ytc.multithread.future;
+package com.ytc.multithread.future.future1;
 
 
 /**
